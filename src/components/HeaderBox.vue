7e6b9da2c5d5105f04090ab0caecd0fe9c1479b7
Samuel Scozzari <s_scozzari@libero.it>
2022-01-31 12:56:53 +0100

    Header section completed

--- src/components/HeaderBox.vue
+++ src/components/HeaderBox.vue
@@ -1,9 +1,28 @@
 <template>
-  <header></header>
+  <header>
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/1/19/Spotify_logo_without_text.svg"
+      alt=""
+    />
+  </header>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+  background-color: #2e3a46;
+  height: 70px;
+  display: flex;
+  align-items: center;
+
+  img {
+    height: 45px;
+    margin-left: 20px;
+    background-color: #191716;
+    border-radius: 50%;
+  }
+}
+</style>
