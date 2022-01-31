92a4ab6fa255ff30851d33cb770eed8d47635f5c
Samuel Scozzari <s_scozzari@libero.it>
2022-01-31 14:07:24 +0100

    CardBox completed

--- src/components/CardBox.vue
+++ src/components/CardBox.vue
@@ -1,22 +1,54 @@
 <template>
   <div class="card-box">
-    <img src="" alt="" />
+    <img :src="disco.poster" alt="" />
 
-    <h2></h2>
+    <h2>{{ disco.title.toUpperCase() }}</h2>
 
-    <div class="name"></div>
-    <div class="date"></div>
+    <div class="name">{{ disco.author }}</div>
+    <div class="date">{{ disco.year }}</div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    disco: Object,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .card-box {
-  height: 370px;
+  height: 365px;
   width: 200px;
+  margin: 10px 20px;
   background-color: #2e3a46;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  img {
+    /* height: 150px; */
+    width: 160px;
+    margin: 20px 0;
+  }
+
+  h2 {
+    color: white;
+    text-align: center;
+    width: 160px;
+    font-size: 22px;
+    padding-bottom: 25px;
+  }
+
+  .name {
+    font-size: 19px;
+    padding-bottom: 4px;
+  }
+
+  .name,
+  .date {
+    color: #808080;
+  }
 }
 </style>
