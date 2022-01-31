4f247b03380546523dcb198d927b19bd3bc6bb9d
Samuel Scozzari <s_scozzari@libero.it>
2022-01-31 13:10:17 +0100

    Added card base, changed file name

---
+++ src/components/CardBox.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card-box">
+    <img src="" alt="" />
+
+    <h2></h2>
+
+    <div class="name"></div>
+    <div class="date"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.card-box {
+  height: 370px;
+  width: 200px;
+  background-color: #2e3a46;
+}
+</style>
